a57106bc65a87001ec233cd80bdab2cd71082dac
TranTrung3204 <trung030204@gmail.com>
2024-12-04 00:01:43 +0700

    Upddate

---
+++ mysa/saleapp/utils.py
@@ -0,0 +1,12 @@
+import json, os
+from saleapp import app
+# ham doc file json
+def read_json(path):
+    with open(path, "r") as f:
+        return json. load(f)
+#ham doc categories
+def load_categories():
+    return read_json(os.path.join(app.root_path, 'data/categories.json'))
+
+def load_products():
+    return read_json(os.path.join(app.root_path, 'data/products.json'))
